30ff79ae3ff150721513828472a8c0d309517606
Mario Orlandi <morlandi@brainstorm.it>
2022-01-25 08:47:13 +0100

    ...

--- scripts/run_mysql.bash
+++ scripts/run_mysql.bash
@@ -1,1 +1,4 @@
+# See:
+# https://hub.docker.com/r/mysql/mysql-server
+
 docker run -d --name mysql1 --env MYSQL_ROOT_PASSWORD=password --env MYSQL_ROOT_HOST=% --volume ~/DockerDev/tmp/mysql1:/var/lib/mysql --publish 0.0.0.0:3306:3306 mysql/mysql-server
